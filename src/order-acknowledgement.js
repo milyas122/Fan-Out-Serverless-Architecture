9ed5d66f3b2228f64f5995f60c532f970bb22d5e
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-06-26 15:19:13 +0500

    create subscriptions

--- src/order-acknowledgement.js
+++ src/order-acknowledgement.js
@@ -1,18 +1,64 @@
 "use strict";
 
-module.exports.hello = async (event) => {
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
+const { v4: uuidv4 } = require("uuid");
+const moment = require("moment");
+const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
+
+const dynamodb_client = new DynamoDBClient({ region: "us-east-1" });
+const sns_client = new SNSClient({ region: "us-east-1" });
+const ordersTable = process.env.ORDER_TABLE;
+const topicArn = process.env.SNS_TOPIC_ARN;
+
+module.exports.handler = async (event) => {
+  const data = JSON.parse(event.body);
+  // console.log(event);
+  // const data = event;
+  const id = uuidv4().toString();
+  await createOrder({ id, ...data });
+  console.log("fine");
+  const sns_command = new PublishCommand({
+    TopicArn: topicArn,
+    Message: JSON.stringify({ id, ...data }),
+    MessageAttributes: {
+      orderType: {
+        DataType: "String",
+        StringValue: data.orderType,
+      },
+    },
+  });
+  const sns_response = await sns_client.send(sns_command);
+  console.log("SNS Response: ", sns_response);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
+        message: "success",
+        data: {},
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+async function createOrder({ id, productName, quantity, price, orderType }) {
+  const total = price * quantity;
+  const params = {
+    TableName: ordersTable,
+    Item: marshall({
+      id,
+      type: orderType,
+      status: "pending",
+      created_at: moment().format(),
+      updated_at: moment().format(),
+      productName,
+      quantity,
+      total,
+    }),
+  };
+  const command = new PutItemCommand(params);
+  await dynamodb_client.send(command);
+}
--- src/order-shipment.js
+++ src/order-shipment.js
@@ -1,18 +1,17 @@
 "use strict";
 
-module.exports.hello = async (event) => {
+module.exports.handler = async (event) => {
+  console.log("Event: ", event);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
+        message: "Physical Order is processed",
+        data: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
